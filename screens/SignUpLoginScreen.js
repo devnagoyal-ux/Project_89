fd48605ae77bf2abb8c96b7c05a070d6724db000
devnagoyal-ux <58598904+devnagoyal-ux@users.noreply.github.com>
2021-01-04 12:55:58 +0530

    added styling

--- screens/SignUpLoginScreen.js
+++ screens/SignUpLoginScreen.js
@@ -2,6 +2,8 @@ import React,{Component} from 'react';
 import { View, StyleSheet, Text, TouchableOpacity,TextInput, Alert,Modal,ScrollView,KeyboardAvoidingView } from 'react-native';
 import firebase from 'firebase';
 import db from '../config';
+import { RFValue } from "react-native-responsive-fontsize";
+
 
 export default class SignUpLogin extends Component{
     constructor(){
@@ -62,120 +64,140 @@ export default class SignUpLogin extends Component{
         })
       }
 
-      showModal = ()=>{
-        return(
-        <Modal
-          animationType="fade"
-          transparent={true}
-          visible={this.state.isModalVisible}
+      showModal = () => {
+        return (
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={this.state.isModalVisible}
           >
-          <View style={styles.modalContainer}>
-            <ScrollView style={{width:'100%'}}>
-              <KeyboardAvoidingView style={styles.KeyboardAvoidingView}>
-              <Text
-                style={styles.modalTitle}
-                >Registration</Text>
-              <TextInput
-                style={styles.formTextInput}
-                placeholder ={"First Name"}
-                maxLength ={10}
-                onChangeText={(text)=>{
-                  this.setState({
-                    firstName: text
-                  })
-                }}
-              />
-              <TextInput
-                style={styles.formTextInput}
-                placeholder ={"Last Name"}
-                maxLength ={12}
-                onChangeText={(text)=>{
-                  this.setState({
-                    lastName: text
-                  })
-                }}
-              />
-              <TextInput
-                style={styles.formTextInput}
-                placeholder ={"Phone Number"}
-                maxLength ={10}
-                keyboardType={'numeric'}
-                onChangeText={(text)=>{
-                  this.setState({
-                    contact: text
-                  })
-                }}
-              />
-              <TextInput
-                style={styles.formTextInput}
-                placeholder ={"Address"}
-                multiline = {true}
-                onChangeText={(text)=>{
-                  this.setState({
-                    address: text
-                  })
-                }}
-              />
-              <TextInput
-              style={styles.formTextInput}
-              placeholder={"Country Currency Code"}
-              maxLength={8}
-              onChangeText={(text)=>{
-                this.setState({
-                  currencyCode: text
-                })
-              }}
-            />
-              <TextInput
-                style={styles.formTextInput}
-                placeholder ={"Email"}
-                keyboardType ={'email-address'}
-                onChangeText={(text)=>{
-                  this.setState({
-                    emailId: text
-                  })
-                }}
-              /><TextInput
-                style={styles.formTextInput}
-                placeholder ={"Password"}
-                secureTextEntry = {true}
-                onChangeText={(text)=>{
-                  this.setState({
-                    password: text
-                  })
-                }}
-              /><TextInput
-                style={styles.formTextInput}
-                placeholder ={"Confirm Password"}
-                secureTextEntry = {true}
-                onChangeText={(text)=>{
-                  this.setState({
-                    confirmPassword: text
-                  })
-                }}
-              />
-              <View style={styles.modalBackButton}>
-                <TouchableOpacity
-                  style={styles.registerButton}
-                  onPress={()=>
-                    this.userSignUp(this.state.emailId, this.state.password, this.state.confirmPassword)
-                  }
-                >
-                <Text style={styles.registerButtonText}>Register</Text>
-                </TouchableOpacity>
+            <ScrollView style={styles.scrollview}>
+              <View style={styles.signupView}>
+                <Text style={styles.signupText}> SIGN UP </Text>
               </View>
-              <View style={styles.modalBackButton}>
-                <TouchableOpacity
-                  style={styles.cancelButton}
-                  onPress={()=>this.setState({"isModalVisible":false})}
-                >
-                <Text style={{color:'#ff5722'}}>Cancel</Text>
-                </TouchableOpacity>
-              </View>
-              </KeyboardAvoidingView>
+                  <View style={{flex:0.95}}>
+                      <Text style={styles.label}>First Name </Text>
+                      <TextInput
+                        style={styles.formInput}
+                        placeholder={"First Name"}
+                        maxLength={12}
+                        onChangeText={(text) => {
+                          this.setState({
+                            firstName: text,
+                          });
+                        }}
+                      />
+    
+                      <Text style={styles.label}>Last Name </Text>
+                      <TextInput
+                        style={styles.formInput}
+                        placeholder={"Last Name"}
+                        maxLength={12}
+                        onChangeText={(text) => {
+                          this.setState({
+                            lastName: text,
+                          });
+                        }}
+                      />
+
+                     <Text style={styles.label}>Currency Code </Text>
+                     <TextInput
+                       style={styles.formInput}
+                       placeholder={"Country Currency Code"}
+                       maxLength={8}
+                       onChangeText={(text)=>{
+                       this.setState({
+                       currencyCode: text
+                          });
+                        }}
+                     />
+    
+                      <Text style={styles.label}>Contact </Text>
+                      <TextInput
+                        style={styles.formInput}
+                        placeholder={"Contact"}
+                        maxLength={10}
+                        keyboardType={"numeric"}
+                        onChangeText={(text) => {
+                          this.setState({
+                            contact: text,
+                          });
+                        }}
+                      />
+    
+                      <Text style={styles.label}> Address </Text>
+                      <TextInput
+                        style={styles.formInput}
+                        placeholder={"Address"}
+                        multiline={true}
+                        onChangeText={(text) => {
+                          this.setState({
+                            address: text,
+                          });
+                        }}
+                      />
+    
+                      <Text style={styles.label}>Email </Text>
+                      <TextInput
+                        style={styles.formInput}
+                        placeholder={"Email"}
+                        keyboardType={"email-address"}
+                        onChangeText={(text) => {
+                          this.setState({
+                            emailId: text,
+                          });
+                        }}
+                      />
+    
+                      <Text style={styles.label}> Password </Text>
+                      <TextInput
+                        style={styles.formInput}
+                        placeholder={"Password"}
+                        secureTextEntry={true}
+                        onChangeText={(text) => {
+                          this.setState({
+                            password: text,
+                          });
+                        }}
+                      />
+    
+                      <Text style={styles.label}>Confirm Password</Text>
+                      <TextInput
+                        style={styles.formInput}
+                        placeholder={"Confrim Password"}
+                        secureTextEntry={true}
+                        onChangeText={(text) => {
+                          this.setState({
+                            confirmPassword: text,
+                          });
+                        }}
+                      />
+                  </View>
+    
+                <View style={{flex:0.2,alignItems:'center'}}>
+                  <TouchableOpacity
+                    style={styles.registerButton}
+                    onPress={() =>
+                      this.userSignUp(
+                        this.state.emailId,
+                        this.state.password,
+                        this.state.confirmPassword
+                      )
+                    }
+                  >
+                    <Text style={styles.registerButtonText}>Register</Text>
+                  </TouchableOpacity>
+                  <Text
+                   style={styles.cancelButtonText}
+                   onPress={() => {
+                     this.setState({ isModalVisible: false });
+                   }}
+                  >
+                  Cancel
+                  </Text>
+                </View>
             </ScrollView>
-          </View>
-        </Modal>
+          </Modal>
     
     )
     }
@@ -183,16 +205,16 @@ export default class SignUpLogin extends Component{
     render(){
         return(
           <View style={styles.container}>
-          <View style={{justifyContent: 'center',alignItems: 'center'}}>
+          {this.showModal()}
+          <View
+            style={{ flex: 0.25}}
+          >
+          <View style={{flex:0.15}}/>
+          <View style={styles.santaView}> Barter App</View>
+          </View>
+          <View style={{ flex: 0.45 }}>
   
-          </View>
-            {
-              this.showModal()
-            }
-          <View style={{justifyContent:'center', alignItems:'center'}}>
-            <Text style={styles.title}>Barter App</Text>
-          </View>
-          <View>
+            <View style={styles.TextInput}>
               <TextInput
               style={styles.loginBox}
               placeholder="abc@example.com"
@@ -213,6 +235,7 @@ export default class SignUpLogin extends Component{
               })
             }}
           />
+          </View>
         
           <TouchableOpacity
              style={[styles.button,{marginBottom:20, marginTop:20}]}
@@ -230,61 +253,130 @@ export default class SignUpLogin extends Component{
              <Text style={styles.buttonText}>SignUp</Text>
            </TouchableOpacity>
         </View>
+        
       </View>
       )
     }
   }
+ 
   
   
   const styles = StyleSheet.create({
-    container:{
-      flex:1,
-      backgroundColor:'#F8BE85'
+    container: {
+      flex: 1,
+      backgroundColor: "#6fc0b8",
     },
-    profileContainer:{
-      flex:1,
-      justifyContent:'center',
-      alignItems:'center',
+    loginBox: {
+      width: "80%",
+      height: RFValue(50),
+      borderWidth: 1.5,
+      borderColor: "#ffffff",
+      fontSize: RFValue(20),
+      paddingLeft: RFValue(10),
     },
-    title :{
-      fontSize:65,
-      fontWeight:'300',
-      paddingBottom:30,
-      color : '#ff3d00'
-    },
-    loginBox:{
-      width: 300,
-      height: 40,
-      borderBottomWidth: 1.5,
-      borderColor : '#ff8a65',
-      fontSize: 20,
-      margin:10,
-      paddingLeft:10
-    },
-    button:{
-      width:300,
-      height:50,
-      justifyContent:'center',
-      alignItems:'center',
-      borderRadius:25,
-      backgroundColor:"#ff9800",
+    button: {
+      width: "80%",
+      height: RFValue(50),
+      justifyContent: "center",
+      alignItems: "center",
+      borderRadius: RFValue(25),
+      backgroundColor: "#ffff",
       shadowColor: "#000",
+      marginBottom:RFValue(10),
       shadowOffset: {
-         width: 0,
-         height: 8,
+        width: 0,
+        height: 8,
       },
-      shadowOpacity: 0.30,
+      shadowOpacity: 0.3,
       shadowRadius: 10.32,
       elevation: 16,
     },
-    buttonText:{
-      color:'#ffff',
-      fontWeight:'200',
-      fontSize:20
+    buttonText: {
+      color: "#32867d",
+      fontWeight: "200",
+      fontSize: RFValue(20),
     },
-    buttonContainer:{
-      flex:1,
+    label:{
+      fontSize:RFValue(13),
+      color:"#717D7E",
+      fontWeight:'bold',
+      paddingLeft:RFValue(10),
+      marginLeft:RFValue(20)
+    },
+    formInput: {
+      width: "90%",
+      height: RFValue(45),
+      padding: RFValue(10),
+      borderWidth:1,
+      borderRadius:2,
+      borderColor:"grey",
+      paddingBottom:RFValue(10),
+      marginLeft:RFValue(20),
+      marginBottom:RFValue(14)
+    },
+    registerButton: {
+      width: "75%",
+      height: RFValue(50),
+      marginTop:RFValue(20),
+      justifyContent: "center",
+      alignItems: "center",
+      borderRadius: RFValue(3),
+      backgroundColor: "#32867d",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 8,
+      },
+      shadowOpacity: 0.44,
+      shadowRadius: 10.32,
+      elevation: 16,
+      marginTop: RFValue(10),
+    },
+    registerButtonText: {
+      fontSize: RFValue(23),
+      fontWeight: "bold",
+      color: "#fff",
+    },
+    cancelButtonText:{
+      fontSize : RFValue(20),
+      fontWeight:'bold',
+      color: "#32867d",
+      marginTop:RFValue(10)
+    },
+    scrollview:{
+      flex: 1,
+      backgroundColor: "#fff"
+    },
+    signupView:{
+      flex:0.05,
+      justifyContent:'center',
       alignItems:'center'
-    }
-  })
+  },
+  signupText:{
+    fontSize:RFValue(20),
+    fontWeight:"bold",
+    color:"#32867d"
+  },
+  santaView:{
+    flex:0.85,
+    justifyContent:"center",
+    alignItems:"center",
+    padding:RFValue(10),
+    fontSize:RFValue(20)
+  },
+  santaImage:{
+    width:"70%",
+    height:"100%",
+    resizeMode:"stretch"
+  },
+  TextInput:{
+    flex:0.5,
+    alignItems:"center",
+    justifyContent:"center"
+  },
+  bookImage:{
+    width:"100%",
+    height:RFValue(220)
+  }
+  });
   
